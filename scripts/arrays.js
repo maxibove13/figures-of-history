3bc1e068f8d7071ed671e8d3c3f69efb3f9ee970
Max <maxibove13@gmail.com>
2020-09-01 11:17:47 -0300

    footer stylized

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -86,3 +86,19 @@ for (let indexCarouselItems = 0; indexCarouselItems < emperors.length; indexCaro
     const container = document.querySelector(".carousel-inner")
 
 }
+
+
+//Get this year//
+const today = new Date();
+thisYear = today.getFullYear();
+document.getElementById('company').innerHTML = '&copy;' + thisYear + ' logos'
+
+
+//Activate & deactivate Overlay//
+function onOverlay() {
+    document.getElementById('watch-out-overlay').style.display='block';
+}
+
+function offOverlay() {
+    document.getElementById('watch-out-overlay').style.display='none';
+}
